a5421624a1ff3260aead8c8658967ba19574add5
Analih Brum Ineu <analihineu@hotmail.com>
2025-06-27 16:28:30 -0300

    Create ordenar.py

---
+++ internal-sorting/bubble-sort/exercises/Veiculo/ordenar.py
@@ -0,0 +1,15 @@
+from armazenar import armazenar
+
+def ordenar(veiculos):
+    # Bubble Sort decrescente com base no ano do veículo
+    for i in range(len(veiculos) - 1, 0, -1):
+        for j in range(i):
+            if veiculos[j].get_ano() < veiculos[j + 1].get_ano():
+                temp = veiculos[j]
+                veiculos[j] = veiculos[j + 1]
+                veiculos[j + 1] = temp
+
+    for v in veiculos:
+        print(f"{v.get_ano()} - {v.get_marca()} - {v.get_cor()}")
+
+    return veiculos
