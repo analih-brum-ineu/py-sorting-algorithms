2b6f96568a74db33ca2e32cd431ece032126dae8
Analih Brum Ineu <analihineu@hotmail.com>
2025-06-27 16:27:41 -0300

    Create Veiculo.py

---
+++ internal-sorting/bubble-sort/exercises/Veiculo/Veiculo.py
@@ -0,0 +1,14 @@
+class Veiculo:
+    def __init__(self, marca, cor, ano):
+        self.marca = marca
+        self.cor = cor
+        self.ano = ano
+
+    def get_marca(self):
+        return self.marca
+
+    def get_cor(self):
+        return self.cor
+
+    def get_ano(self):
+        return self.ano
